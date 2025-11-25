ed88f33bb04a058cc20fc946d0f7b8849a86cefc
Joe <Joe@DESKTOP-K8AJFA1>
2025-11-25 15:55:48 -0800

    generic item class added

--- Items.java
+++ Items.java
@@ -1,2 +1,17 @@
-public class Items {
+public class Item {
+	private boolean isConsumable;
+	private int delta;
+	
+	public Item (boolean consumable, int delta) {
+		isConsumable = consumable;
+		this.delta = delta;
+	}
+	
+	public void use(Dice d) {
+		if isConsumable{
+			selection = 1;  //todo replace value with player selected die face index
+			d.changeFace(selection, delta);
+			return;
+		}
+	}
 }
