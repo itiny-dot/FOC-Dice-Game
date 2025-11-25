16cc85d452f45fbf0d5d09d61de298f02b9307cb
Joe <Joe@DESKTOP-K8AJFA1>
2025-11-25 13:07:37 -0800

    made it possible to edit die faces using a changeFace method.

--- Dice.java
+++ Dice.java
@@ -3,11 +3,21 @@ import java.util.Random;
 public class Dice {
     static Random random = new Random();
     //rolls dice, int 1-6 corresponding to it's initial roll value (prior to buffs)
+    private int faces[6]=[1,2,3,4,5,6];
+    //sets default faces for a die to default to base die values
     public static int rollDice(int dice){
         int rollValue = random.nextInt(6);
-        //add 1 to the random value as it's range is index 0-5; this is for convenience.
-        rollValue += 1;
         //System.out.println(rollValue);
-        return rollValue;
+        return faces[rollValue];
+        //random value picks the index of a given face of a die to allow for die editing
+    }
+    public static boolean changeFace(int face,int delta) {
+    	//changes die face by a delta amount provided result is a positive int
+    	if (faces[face]+delta>0) {
+    		faces[face]+=delta;
+    		return true;
+    		else return false;
+    	}
+    	
     }
 }
