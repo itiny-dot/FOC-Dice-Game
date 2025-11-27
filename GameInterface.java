2faf7288ecc9afe191456b9f540487bbeeb9232e
Rich Ngo <135562502+RichNgo@users.noreply.github.com>
2025-11-26 16:24:48 -0800

    Updated Interface
    Made the screen 1920x1024.
    Added a border around where the dice will go. 
    Added runtime.

--- GameInterface.java
+++ GameInterface.java
@@ -1,20 +1,60 @@
 package main;
-import javax.swing.JPanel;
 import java.awt.*;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JOptionPane;
 
-public class GameInterface extends JPanel{
+public class GameInterface extends JPanel implements Runnable {
     // SCREEN SETTINGS
-    final int originalTileSize = 16;
-    final int scale = 3;//16x16 tile
+    final int originalTileSize = 16;//64x64tile size
+    final int scale = 4; //Multiplier for how large tiles appear on screen
     final int tileSize = originalTileSize * scale;
-    final int maxScreenCol = 16;
-    final int maxScreenRow = 12;
-    final int screenWidth = tileSize * maxScreenCol; //768 pixels
-    final int screenHeight = tileSize * maxScreenRow; //576 pixels
+    final int maxScreenCol = 30;
+    final int maxScreenRow = 16;
+    final int screenWidth = tileSize * maxScreenCol; //1920 pixels
+    final int screenHeight = tileSize * maxScreenRow; //1024 pixels
 
-    public GameInterface(){
+    Thread gameThread;
+
+
+    public GameInterface() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
-        this.setBackground(Color.green);
+        Color customColor1 = new Color(0, 100, 0); //Dark Green background color
+        this.setBackground(customColor1);
         this.setDoubleBuffered(true);
+    }
+
+    //Game runtime
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        while (gameThread != null) {
+            //Update Frame for animations
+            update();
+            //Redraws the screen each frame change
+            repaint();
+        }
+    }
+
+    public void update() {
+
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        //Outer border of box that holds the dice (temporary)
+        g2.setColor(Color.white);
+        g2.fillRect(100, 100, 10, 300); //Left Border
+        g2.fillRect(1820, 100, 10, 300); //Right Border
+        g2.fillRect(100, 100, 1720, 10); //Top Border
+        g2.fillRect(100, 400, 1730, 10); //Bottom Border
+        
     }
 }
